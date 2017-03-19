5937a9cd15d48e52e87f70268a4bd8866ab492a0
Ying Tai <taiying@cse.msu.edu>
2017-03-20 00:19:51 +0800

    1

---
+++ caffe_files/DRRN_B1U9_20C128/train_DRRN_B1U9_20C128.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+LOG=./log/DRRN_B1U9_20C128_291_31.log
+CAFFE=/data2/taiying/MSU_Code/my_caffe/build/tools/caffe # your caffe path
+
+$CAFFE train --solver=./DRRN_B1U9_20C128_solver.prototxt -gpu 0 2>&1 | tee $LOG
+
+## resume training
+#$CAFFE train --solver=./DRRN_B1U9_20C128_solver.prototxt \
+#--snapshot=../../model/DRRN_B1U9_20C128_iter_6536.solverstate -gpu 0 2>&1 | tee $LOG
+
